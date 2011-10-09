3e7a5a7f2895659a8e66958397f86e13c754d9f7
Rolf Leggewie <foss@rolf.leggewie.biz>
2011-10-10 01:02:35 +0800

    leverage more of debhelper to do the packaging
    * clean target is now fully in sync with dh7
    * treat example and other documentation files properly
    * less of fragile scripting (ongoing effort)

--- debian/finish-install-first.sh
+++ debian/finish-install-first.sh
@@ -2,39 +2,14 @@
 
 	set -e
 
-	install -m 0755 scripts/isdncause                   debian/isdnutils-base/usr/bin/isdncause
-	install -m 0644 isdnctrl/README.libipt              debian/isdnutils-base/usr/share/doc/isdnutils-base/
 	rm -f             debian/tmp/etc/isdn/eftusers
-	# ln -s ../ftpusers debian/isdneurofile/etc/isdn/eftusers
-	install -m 0444 Mini-FAQ/isdn-faq.*           debian/isdnutils-doc/usr/share/doc/isdnutils-doc/Mini-FAQ/
-	mv debian/tmp/usr/bin/x*                      debian/isdnutils-xtools/usr/bin/
-	cp xmonisdn/README                            debian/isdnutils-xtools/usr/share/doc/isdnutils-xtools/README.xmonisdn
-	# move some extra stuff from upstream source into debian tree
-	cp ipppd/NOTES.IPPPD ipppd/README* debian/README.MPPP debian/ipppd/usr/share/doc/ipppd/
-	mkdir -p                                      debian/isdnlog/usr/share/doc/isdnlog
-	cp lib/README.Syntax.conffile                 debian/isdnlog/usr/share/doc/isdnlog/Syntax.conffile.de
-	cp isdnlog/BUGS	                              debian/isdnlog/usr/share/doc/isdnlog/BUGS.de
-	cp isdnlog/FAQ	                              debian/isdnlog/usr/share/doc/isdnlog/FAQ.de
-	cp isdnlog/README                             debian/isdnlog/usr/share/doc/isdnlog/README.de
-	cp isdnlog/TODO	                              debian/isdnlog/usr/share/doc/isdnlog/TODO.de
-	cp isdnlog/contrib/winpopup/winpopup          debian/isdnlog/usr/share/doc/isdnlog/examples/winpopup.de
-	cp isdnlog/samples/*                          debian/isdnlog/usr/share/isdn/default/
-	rm -f debian/isdnlog/usr/share/isdn/default/*.in
 
 	# not this one
 	rm -f debian/isdnlog/usr/share/isdn/default/callerid.conf
 	rm -f debian/tmp/etc/isdn/callerid.conf	\
 	      debian/tmp/etc/isdn/isdn.conf	\
 	      debian/tmp/etc/isdn/rate.conf
-	# cp pcbit/README.pt                          debian/isdnactivecards/usr/share/doc/isdnactivecards/README.pcbit.pt
-	# extra stuff, not originating from the upstream source
-	install -m 0755 debian/isdnconfig           debian/isdnutils-base/usr/sbin/
-	install -m 0644 debian/isdnconfig.8         debian/isdnutils-base/usr/share/man/man8/
-	install -m 0755 debian/x*-net*	            debian/isdnutils-xtools/etc/isdn/
-	install -m 0644 debian/init.d.functions     debian/isdnutils-base/etc/isdn/
-	install -m 0444 debian/ioptions		    debian/ipppd/etc/ppp/
-	install -m 0755 debian/ip-down.d            debian/ipppd/etc/ppp/ip-down.d/99-ipppd
-	install -m 0755 debian/ip-up.d	            debian/ipppd/etc/ppp/ip-up.d/00-ipppd
+
 	rmdir debian/isdnvboxserver/usr/share/doc/isdnvboxserver/examples || true # dont ask
 	mv debian/tmp/usr/share/doc/isdnvboxserver/* debian/isdnvboxserver/usr/share/doc/isdnvboxserver/.
 	rmdir debian/tmp/usr/share/doc/isdnvboxserver
@@ -43,9 +18,6 @@
 	for A in debian/ipppd.DEVICE \
 		debian/auth-down debian/auth-up ; do \
 	    cp $A debian/ipppd/usr/share/isdn/default/; \
-	done
-	for A in debian/callerid.conf debian/isdnlog.DEVICE; do \
-	    cp $A debian/isdnlog/usr/share/isdn/default/; \
 	done
 	mkdir debian/isdnvboxserver/usr/share/doc/isdnvboxserver/examples || true # dont ask
         # The $A's below need to become $$A if this code is placed back into the makefile.
@@ -59,7 +31,6 @@
 	install -m 0644 debian/HOWTO	     debian/isdnutils-base/usr/share/doc/isdnutils-base/
 	ln -s HOWTO                          debian/isdnutils-base/usr/share/doc/isdnutils-base/CONFIG
 	ln -s HOWTO                          debian/isdnutils-base/usr/share/doc/isdnutils-base/HOWTO.isdnutils
-	install -m 0644 debian/README.HiSax  debian/isdnutils-base/usr/share/doc/isdnutils-base/
 	install -m 0644 debian/README.source debian/isdnutils-base/usr/share/doc/isdnutils-base/
 	install -m 0444 debian/ioptions      debian/ipppd/etc/ppp/
 	install -m 0644 isdnlog/rate-*.dat                 debian/isdnlog-data/usr/share/isdn/
