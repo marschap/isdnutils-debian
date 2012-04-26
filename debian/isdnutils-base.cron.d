50ee9340b49c01916e1be14bdc1f0a60828c8971
Rolf Leggewie <foss@rolf.leggewie.biz>
2012-04-26 16:11:55 +0800

    isdnutils-base: use dh_installcron instead of postinst. Closes: #197254
    - this fixes an RC-level severity problem for isdnutils-base
      the problem with isdnlog and isdn.conf still needs to be dealt with

---
+++ debian/isdnutils-base.cron.d
@@ -0,0 +1,5 @@
+# Change the line below for your local requirements and uncomment them.
+# Use "init q" to reread inittab.
+# look at the mgetty manpage for more information (mgetty isn't standard!)
+#
+#I0:2345:respawn:/sbin/mgetty -D -m '"" ATZ OK AT&Eyourmsnhere OK AT&B512 OK' -s 38400 ttyI0
